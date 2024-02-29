6f4ace75cad728acce28f077398c8354335ae076
dlgusdn978 <dlgusdn978@naver.com>
2024-02-29 19:06:02 +0900

    Feat(FE) : checkout init design

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -102,13 +102,15 @@ export default function Page() {
         <h1 className="font-bold text-xl">주문자 정보</h1>
         <table className="border-t-2 w-full">
           <colgroup>
-            <col className="w-1/4 py-5 bg-gray-100"></col>
+            <col className="w-1/4 py-5 mx-2 bg-gray-100"></col>
             <col className="w-3/4"></col>
           </colgroup>
 
-          <tbody className="border-t-2 border-black">
-            <tr className="">
-              <th className="py-5">성함</th>
+          <tbody className="border-t-2 border-black [&>*]:border-b-2 [&>*]:border-slate-200">
+            <tr className="ml-5">
+              <th className="py-5 mx-2">
+                <label>성함</label>
+              </th>
               <td>
                 <input
                   type="text"
@@ -162,7 +164,7 @@ export default function Page() {
             <col className="w-1/4 bg-gray-100"></col>
             <col className="w-3/4"></col>
           </colgroup>
-          <tbody>
+          <tbody className="[&>*]:border-b-2 [&>*]:border-slate-200">
             <tr>
               <th className="py-5">배송지 선택</th>
               <td>기본 배송지</td>
@@ -214,6 +216,153 @@ export default function Page() {
             </tr>
           </tbody>
         </table>
+      </div>
+      <div className="flex flex-row justify-between">
+        <div className="w-7/12">
+          <h1 className="font-bold text-xl">결제 수단</h1>
+          <div className="border-t-2 border-black p-5 [&>*]:mr-5 [&>*]:[&>*]:mr-2">
+            <form>
+              <span>
+                <input
+                  type="radio"
+                  id="card"
+                  name="payment"
+                  value="card"
+                ></input>
+                <label htmlFor="card">신용카드</label>
+              </span>
+              <span>
+                <input
+                  type="radio"
+                  id="account"
+                  name="payment"
+                  value="account"
+                ></input>
+                <label htmlFor="account">계좌이체</label>
+              </span>
+              <span>
+                <input
+                  type="radio"
+                  id="kakao"
+                  name="payment"
+                  value="kakao"
+                ></input>
+                <label htmlFor="kakao">카카오페이</label>
+              </span>
+              <span>
+                <input
+                  type="radio"
+                  id="naver"
+                  name="payment"
+                  value="naver"
+                ></input>
+                <label htmlFor="naver">네이버페이</label>
+              </span>
+              <span>
+                <input
+                  type="radio"
+                  id="toss"
+                  name="payment"
+                  value="toss"
+                ></input>
+                <label htmlFor="toss">토스페이</label>
+              </span>
+            </form>
+          </div>
+        </div>
+        <div className="w-4/12">
+          <h1 className="font-bold text-xl">결제 금액</h1>
+          <div className="bg-slate-50 border-2 p-8">
+            <ul className="[&>*]:mb-6 [&>*]:flex [&>*]:justify-between border-b-2 border-black">
+              <li>
+                <span>총 주문 금액</span>
+                <span>원</span>
+              </li>
+              <li>
+                <span>쿠폰</span>
+                <span>원</span>
+              </li>
+              <li>
+                <span>포인트</span>
+                <span>원</span>
+              </li>
+              <li>
+                <span>배송비</span>
+                <span>원</span>
+              </li>
+              <li>
+                <span>배송비 할인</span>
+                <span>원</span>
+              </li>
+              <li>
+                <span>포장비</span>
+                <span>원</span>
+              </li>
+            </ul>
+            <div className="pt-5 flex justify-between">
+              <strong className="font-bold text-xl">최종 결제금액</strong>
+              <span className="font-bold text-2xl text-orange-500">
+                <b>7300원</b>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <h1 className="font-bold text-xl">약관 동의</h1>
+        <div className="border-t-2 border-black [&>*]:p-5">
+          <div className="border-b-[1px] border-gray-200">
+            <span className="flex items-center">
+              <input
+                type="checkbox"
+                className="w-8 h-8 mr-2"
+                name="agreeAll"
+              ></input>
+              <label htmlFor="agreeAll">
+                <strong>전체 동의</strong>
+              </label>
+            </span>
+          </div>
+          <ul className="border-b-[1px] border-gray-200">
+            <li>
+              <span className="flex items-center">
+                <input
+                  type="checkbox"
+                  className="w-8 h-8 mr-2"
+                  name="agree1"
+                ></input>
+                <label htmlFor="agree1" className="text-gray-500">
+                  주문의 상품,가격,배송정보, 할인내역 등을 최종 확인하였으며,
+                  구매에 동의합니다. (필수) (전자상거래법 제 8조 제 2항)
+                </label>
+              </span>
+            </li>
+            <li className="mt-2">
+              <span className="flex items-center">
+                <input
+                  type="checkbox"
+                  className="w-8 h-8 mr-2"
+                  name="agree2"
+                ></input>
+                <label htmlFor="agree2" className="text-gray-500">
+                  개인정보 수집 동의 (필수)
+                </label>
+              </span>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="[&>*]:w-72 [&>*]:h-20 flex justify-center">
+        <input
+          type="button"
+          value="결제하기"
+          className="bg-orange-500 font-bold text-2xl text-white mr-5"
+        ></input>
+        <input
+          type="button"
+          value="장바구니로 가기"
+          className="bg-gray-400 font-bold text-2xl text-white"
+        ></input>
       </div>
       {openPostcode && (
         <div className="absolute top-0 w-full h-full bg-black z-10 opacity-50"></div>
